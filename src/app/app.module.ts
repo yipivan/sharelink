c2a074d293bdb7c30ddb859594dfc8fed239f3eb
Ivan Yip <ivan_yip@msn.com>
2017-12-31 15:56:37 +0800

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule,  } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LinkComponent } from './components/link/link.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LinkListComponent } from './components/link-list/link-list.component';
+import { AddLinkComponent } from './components/add-link/add-link.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    LinkComponent,
+    NavbarComponent,
+    LinkListComponent,
+    AddLinkComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
